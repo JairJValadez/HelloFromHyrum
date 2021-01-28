ceb8a638099b3740070b5f50b97812bf684884b9
elwo19002 <78120155+elwo19002@users.noreply.github.com>
2021-01-27 23:22:11 -0700

    Update Main.java
    Test

--- Main.java
+++ Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Hyrum Elwood.");
-	System.out.println("Hey Hyrum, my name is Karly Gay");
     }
 }
